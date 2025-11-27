946cd44c4b9c5f7fd43bca63d4ddb51c2823c7cb
zubkovns10 <zubkovns10@xmail.ru>
2025-11-27 12:49:21 +0900

    init collector service

---
+++ services/collector/cmd/service/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("ok"))
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
